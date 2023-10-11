eba34ea0805d29de7a3870c7f4812e6e4155efca
Vinicius Pereirx <viniciuspereiraa51@gmail.com>
2023-10-11 14:32:21 -0300

    Client Registration FullStack

---
+++ main.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const moongose = require("mongoose");
+const session = require("express-session");
+
+const app = express();
+
+// Conectando ao MongoDB
+require("./database/db");
+
+// Middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(
+  session({
+    secret: "my secret key",
+    saveUninitialized: true,
+    resave: false,
+  })
+);
+
+app.use((req, res, next) => {
+  res.locals.message = req.session.message;
+  delete req.session.message;
+  next();
+});
+
+// Inserindo template engine
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(3000, () => {
+  console.log("Servidor Ligado!");
+});
---
+++ models/clients.js
@@ -0,0 +1,10 @@
+const moongose = require("mongoose");
+const clientSchema = new moongose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: String, required: true },
+  image: { type: String, required: true },
+  created: { type: Date, required: true, default: Date.now },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+});
